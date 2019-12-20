1ba0b9edb162832f0236c29ce470ea45eb049f59
Jatin Sharma <30732342+rjatin007@users.noreply.github.com>
2019-12-20 14:03:30 +0530

    removed console logs

--- App/ProductApp/src/store/API.js
+++ App/ProductApp/src/store/API.js
@@ -2,10 +2,5 @@ const url = "http://localhost:1337/api/products"; // url
 
 export const fetchData = (page,size) =>
   fetch(`${url}?page=${page}&size=${size}`)
-    .then(res => {
-      console.log(res)
-      return res.json()})
-    .then(res => {
-      console.log(res)
-      return res
-    });
+    .then(res => res.json())
+    .then(res => res);
